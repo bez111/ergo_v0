8571c497401497985a9d68849658e1d3ca7a63b6
“bez” <alexander.bezkrovny@gmail.com>
2025-07-17 12:36:13 -0300

    fix: make Tabs a valid React component for JSX usage

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -3,9 +3,13 @@
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
-const Tabs = TabsPrimitive.Root
+const Tabs = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
+>((props, ref) => <TabsPrimitive.Root ref={ref} {...props} />);
+Tabs.displayName = TabsPrimitive.Root.displayName;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
